cad2a14360752caebfe79cc4e5eefd89268b1e06
sunnaylya <79587455+sunnaylya@users.noreply.github.com>
2025-02-05 20:03:44 +0500

    Add files via upload

---
+++ Js-1_3-prj/task-1/data-types.js
@@ -0,0 +1,20 @@
+const num = 1;
+const str = "Hi";
+const isTrue = true;
+const id = Symbol("id");
+const bigNumber = 10n;
+const empty = null;
+const notDefined = undefined;
+const obj= {
+  name: "Ivan",
+  surName: "Ivanov"
+}
+
+console.log("num", num, typeof num); 
+console.log("str", str, typeof str); 
+console.log("isTrue", isTrue, typeof isTrue); 
+console.log("id", id, typeof id); 
+console.log("bigNumber", bigNumber,typeof bigNumber); 
+console.log("empty", empty, typeof empty); 
+console.log("notDefined", notDefined, typeof notDefined); 
+console.log("obj", obj, typeof obj); 
---
+++ Js-1_3-prj/task-2/index.js
@@ -0,0 +1,10 @@
+let age = 2;
+let height = "3";
+let something = true;
+
+age = "2";
+alert(age);
+height = 3;
+alert(height);
+something = null;
+alert(something);
